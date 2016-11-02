6592f4638f69615d2470573fb1a6403b764d170e
ny8nine <nhamilton1@gmail.com>
2016-11-01 23:19:27 -0400

    basic express app

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const hbl = require('express-handlebars');
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+
+var app = express()
+
+//add template engine
+app.engine('handlebars', hbl({defaultLayout:'main'}))
+//set the view engine to handlebars
+app.set('view engine','handlebars')
+//add middlewear
+app.use(express.static('public'))
+app.use(bodyparser.urlencoded({extended: true}))
+
+app.get('/', function(req, res){
+  res.render('index')
+})
+
+app.listen(3000, function() {
+  console.log('listening on port 3000');
+})
